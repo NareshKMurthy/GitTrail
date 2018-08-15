4d168b1f85f1a6dbd0c0dee14e3964be8e9ef0fa
nareshkmurthy <nareshrk13@gmail.com>
2018-08-15 19:45:02 +0530

    New changes

---
+++ src/Tests/TestDemo2.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class TestDemo2 {
+
+	@Test
+	public void D2(){
+		System.out.println("D5...");
+		System.out.println("D6...");
+		System.out.println("D7...");
+		System.out.println("D8...");		
+	}
+}
